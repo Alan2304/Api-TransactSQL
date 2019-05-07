cc87afa59c33897d4fc8f258cb5b825219c2fd8b
Alan2304 <sanchezalan95@gmail.com>
2019-05-06 20:07:50 -0500

    Add transaction to create a SalesOrderHeader

--- app.js
+++ app.js
@@ -2,13 +2,63 @@ const express = require('express');
 const bodyParse = require('body-parser');
 const sql = require('mssql');
 const db = require('./db');
+const moment = require('moment');
 
 var app = express();
 
 app.use(bodyParse.json());
 
-app.get('/', (req, res) => {
+app.get('/listProducts', (req, res) => {
     res.send("Server Working");
+});
+
+app.post('/listProducts', async (req, res) => {
+    let products = req.body.products;
+    let body = req.body;
+    let dbProducts = [];
+    for (let product of products) {
+        dbProducts.push(await db.getProduct(product.ProductId));
+    }
+    const transaction = new sql.Transaction(await db.db.connect());
+    transaction.begin(err => {
+        let rolledBack = false;
+        transaction.on('rollback', aborted => {
+            rolledBack = true
+        })
+        let query = `INSERT INTO [Sales].[SalesOrderHeader]
+        ([RevisionNumber]
+        ,[OrderDate]
+        ,[DueDate]
+        ,[Status]
+        ,[CustomerID]
+        ,[BillToAddressID]
+        ,[ShipToAddressID]
+        ,[ShipMethodID])
+  VALUES
+        (8
+        ,GETDATE()
+        ,DATEADD(DAY, 7, GETDATE())
+        ,1
+        ,${body.CustomerID}
+        ,1
+        ,1
+        ,1);
+        SELECT SCOPE_IDENTITY() AS id;
+        `
+        new sql.Request(transaction).query(query, (err, result) => {
+            if (err) {
+                if (!rolledBack) {
+                    transaction.rollback(err => {
+                        console.log(err);
+                    })
+                }
+            } else {
+                transaction.commit(err => {
+                    console.log(result.recordset[0].id);
+                })
+            }
+        });
+    });
 });
 
 app.listen(3000, () => {
